0bf11e025d52c7370e770c9c195958694d2c53a7
Panchenko Serhii <panchenko.serhii@hotmail.com>
2024-08-06 12:36:08 +0200

    Fix: Send function

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ let posts = [];
 function sendPost(event) {
   event.preventDefault();
   let postText = document.querySelector("#textPost");
-  document.body.appendChild(document.createElement("div.text")).textContent =
+  document.body.appendChild(document.createElement("div")).textContent =
     postText.value;
   posts.push({
-    id: Date.now,
-    date: Date.toLocaleDateString(),
+    id: Date.now(),
+    date: new Date().toLocaleDateString(),
     text: postText.value,
   });
 
   postText.value = "";
+}
+
+function showPost(post){
+
 }
